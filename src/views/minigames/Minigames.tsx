26a7b54987baed258fe5e13feb724a7012d6ee35
willemhscott <willem.scott@colorado.edu>
2022-03-05 20:13:37 -0700

    Item separators

--- src/views/minigames/Minigames.tsx
+++ src/views/minigames/Minigames.tsx
@@ -1,13 +1,10 @@
 import {Section} from "../../components/paper/Section";
 import React from 'react';
-import {List} from "@mui/material";
 
 export function Minigames() {
     return (
         <Section title={'Minigames'} xs={12}>
-            <List>
-                Minigames
-            </List>
+            Minigames
         </Section>
     )
 }
--- src/views/upgrades/Upgrades.tsx
+++ src/views/upgrades/Upgrades.tsx
@@ -1,13 +1,22 @@
 import {Section} from "../../components/paper/Section";
 import React from 'react';
-import { useGenerators } from "../../hooks/upgrades/Generators";
-import {Avatar, Box, Button, ButtonGroup, IconButton, List, ListItem, ListItemAvatar, ListItemText, Tooltip, useTheme} from "@mui/material";
-import FolderIcon from '@mui/icons-material/Folder';
-import { Upgrade, upgrades } from "../../datatypes/upgrade";
-import { Generator, generators } from "../../datatypes/generator";
-import { Buyable } from "../../datatypes/buyable"
-import { useLinesOfCode } from "../../hooks/stats/LinesOfCode";
-import { BuyButton } from "../../components/button/BuyButton";
+import {useGenerators} from "../../hooks/upgrades/Generators";
+import {
+    Avatar,
+    ButtonGroup,
+    IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Tooltip,
+    useTheme
+} from "@mui/material";
+import {Upgrade, upgrades} from "../../datatypes/upgrade";
+import {Generator, generators} from "../../datatypes/generator";
+import {useLinesOfCode} from "../../hooks/stats/LinesOfCode";
+import {BuyButton} from "../../components/button/BuyButton";
+import {ExtendedTheme} from "../../hooks/styles/Theme";
 
 function UpgradeListItem(props: Upgrade) {
     const upgrade = props;
@@ -16,16 +25,16 @@ function UpgradeListItem(props: Upgrade) {
         <Tooltip title={upgrade.name}>
             <IconButton>
                 <Avatar variant={'rounded'}
-                    sx={{
-                        '& > *': {
-                          width: "15px"  
-                        },
-                        background: "none",
-                        color: theme.palette.primary.light,
-                        '&.Mui-disabled': {
-                            color: theme.palette.primary.main
-                        }
-                    }}
+                        sx={{
+                            '& > *': {
+                                width: "15px"
+                            },
+                            background: "none",
+                            color: theme.palette.primary.light,
+                            '&.Mui-disabled': {
+                                color: theme.palette.primary.main
+                            }
+                        }}
                 >
                     {upgrade.icon}
                 </Avatar>
@@ -40,13 +49,15 @@ function GeneratorListItem(props: Generator) {
     const [generators, setGenerators] = useGenerators()
 
     const [currency, setCurrency] = useLinesOfCode();
-    
-    const buyGenerator = (name: string, value: number, cost: number) => {setGenerators((generator) => {
-        generator.set(name, (generator.get(name) || 0)+ value)
-        setCurrency((oldValue) => oldValue-cost*value);
-        return generator
-    })}
-    
+
+    const buyGenerator = (name: string, value: number, cost: number) => {
+        setGenerators((generator) => {
+            generator.set(name, (generator.get(name) || 0) + value)
+            setCurrency((oldValue) => oldValue - cost * value);
+            return generator
+        })
+    }
+
     const generator = props;
     const generatorUpgrades = upgrades.filter((upgrade: Upgrade) => generator.upgrades.includes(upgrade.name));
     return (
@@ -54,11 +65,13 @@ function GeneratorListItem(props: Generator) {
             <ListItem
                 sx={{width: '100%'}}
                 secondaryAction={
-                    <ButtonGroup variant="outlined" >
-                        <BuyButton onClick={() => buyGenerator(generator.name, 1, generator.cost)} disabled={generator.cost>currency}>
+                    <ButtonGroup variant="outlined">
+                        <BuyButton onClick={() => buyGenerator(generator.name, 1, generator.cost)}
+                                   disabled={generator.cost > currency}>
                             +1
                         </BuyButton>
-                        <BuyButton onClick={() => buyGenerator(generator.name, 10, generator.cost)} disabled={generator.cost*10>currency}>
+                        <BuyButton onClick={() => buyGenerator(generator.name, 10, generator.cost)}
+                                   disabled={generator.cost * 10 > currency}>
                             +10
                         </BuyButton>
                     </ButtonGroup>
@@ -66,19 +79,19 @@ function GeneratorListItem(props: Generator) {
             >
                 <ListItemAvatar>
                     <Avatar variant={'rounded'}
-                        sx={{
-                            width: "50px",
-                            height: "50px",
-                            '& > *': {
-                              width: "42px",
-                              height: "42px"
-                            },
-                            background: "none",
-                            color: theme.palette.primary.light,
-                            '&.Mui-disabled': {
-                                color: theme.palette.primary.main
-                            }
-                        }}
+                            sx={{
+                                width: "50px",
+                                height: "50px",
+                                '& > *': {
+                                    width: "42px",
+                                    height: "42px"
+                                },
+                                background: "none",
+                                color: theme.palette.primary.light,
+                                '&.Mui-disabled': {
+                                    color: theme.palette.primary.main
+                                }
+                            }}
                     >
                         {generator.icon}
                     </Avatar>
@@ -103,9 +116,21 @@ function GeneratorListItem(props: Generator) {
 }
 
 export function GeneratorList() {
+    const theme: ExtendedTheme = useTheme()
+
     return (
         <Section title={'Geneartors'} xs={12}>
-            <List sx={{width: '100%', height: '80vh', overflow: 'scroll'}}>
+            <List sx={{
+                width: '100%', height: '80vh', overflow: 'scroll',
+                '& > :nth-child(even)': {
+                    borderBottom: `1px solid ${theme.palette.outline.main}`,
+                    borderTop: `none`,
+                },
+                '& > :first-child': {
+                    borderBottom: `none`,
+                    borderTop: `1px solid ${theme.palette.outline.main}`,
+                },
+            }}>
                 {generators.map((generator: Generator) => <GeneratorListItem key={generator.name} {...generator} />)}
             </List>
         </Section>
