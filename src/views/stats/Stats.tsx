c2d7d100aaa7db3c4e53dfc4f429f646a90ed4ee
Pumpkintitan <pumpkintitan11@gmail.com>
2022-03-06 10:16:06 -0700

    story works

--- src/views/stats/Stats.tsx
+++ src/views/stats/Stats.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {useLinesOfCode} from "../../hooks/stats/LinesOfCode";
 import {useLinesPerSec} from '../../hooks/upgrades/LinesPerSec'
 import {Grid, TextField, Typography} from "@mui/material";
 import {numberConverter} from "../../utils/numberconverter";
 import {Scroller} from "../typing/Scroller";
+import { useMessages } from '../../hooks/messages/Messages';
+import { story } from '../../datatypes/story'
 
 export function Stats() {
     const [currency, setCurrency] = useLinesOfCode();
     const [lps] = useLinesPerSec();
+    const [messages, setMessages] = useMessages()
+    const [currstory, setCurrstory] = useState(0);
+
+    useEffect(() => {
+        if (currency >= story[currstory].locneeded) {
+            setMessages((me) => [story[currstory].text].concat(me))
+            setCurrstory((c) => c + 1)
+        }
+    
+    }, [currency])
+    
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.value.length >= 10) {
@@ -15,6 +28,8 @@ export function Stats() {
             event.target.value = ""
         }
     };
+
+
 
     return (
         <Grid item xs={12} container padding={2} alignItems={'center'} justifyContent={'space-between'}>
